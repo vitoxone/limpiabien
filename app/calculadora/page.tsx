b92510bd83ad58a66fef0acaeec29789907195a7
Victor <vitox.one@gmail.com>
2026-03-20 11:50:24 -0300

    Update page.tsx

--- app/calculadora/page.tsx
+++ app/calculadora/page.tsx
@@ -170,7 +170,6 @@ export default function CalculatorPage() {
               </>
             )}
 
-            <hr className="sep" />
 
             <div>
               <div className="out-label">Texto para copiar</div>
